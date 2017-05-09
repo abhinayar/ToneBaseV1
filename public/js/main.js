2a1bf6e17c1e96f73a3447e45e9bede3b200234a
Abhi Nayar <abhishek.nayar@yale.edu>
2017-05-09 09:54:34 -0500

    First Commit

---
+++ public/js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+  //Mason Grid
+  /*
+  $("#videoGrid").mason({
+      itemSelector: ".contentBlock",
+      ratio: 1.5,
+      sizes: [
+          [1,1],
+          [1,2],
+          [2,2]
+      ],
+      columns: [
+          [420,840,1260,3]
+      ],
+      promoted: [
+          ['class_name', 2, 1],
+          ['class_name', 2, 3],
+          ['class_name', 3, 3],
+      ],
+      filler: {
+          itemSelector: '.fillerBox',
+          filler_class: 'custom_filler',
+          keepDataAndEvents: false
+      },
+      layout: 'fluid',
+      gutter: 7
+  },function(){
+      alert("COMPLETE!")
+  });
+  */
+});
